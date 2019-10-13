a54a9dc0166d7f5e1fe8f597360438b8953dc84f
dalie <uwe.dalies@zeiss.com>
2019-10-13 10:22:30 +0200

    set project settings zoudalie

--- zoudalie/source/zoudalie/HelloWorld.java
+++ zoudalie/source/zoudalie/HelloWorld.java
@@ -1,9 +1,13 @@
 package zoudalie;
 
+import static java.lang.System.out;
+
+import org.apache.commons.lang.StringEscapeUtils;
+
 public class HelloWorld {
 	
 	public static void main(String[] args) {
-		System.out.println("Hello World");
+		out.print(StringEscapeUtils.escapeHtml("L'indirizzo 1 è obbligatorio. Si prega di indicare una strada. Inserisci una città o APO/FPO."));
 	}
 
 }
