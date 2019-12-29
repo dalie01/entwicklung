b8ff0e82923cfa2bd018d24b254b71519108126a
DALiE <dalie.de@outlook.com>
2019-12-29 12:37:35 +0100

    executorServiceSample

--- oracleOCP/source/dalieOCP/concurrency/executor/_practice/P03.java
+++ oracleOCP/source/dalieOCP/concurrency/executor/_practice/P03.java
@@ -1,44 +1,57 @@
 package dalieOCP.concurrency.executor._practice;
 
-import java.util.Map.Entry;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
-public class P03 {
-	ConcurrentHashMap<String, String> map = new ConcurrentHashMap<>();
-	
-	private void putToMap(String key, String value ) {
+public class P03 implements Runnable {
+	ConcurrentHashMap<Integer, String> map;
+	private int key; 
+	String value;
+
+	P03(ConcurrentHashMap<Integer, String> map, int key, String value) {
+		this.map = map;
+		this.key = key;
+		this.value = value;
+	}
+
+	@Override
+	public void run() {
 		try {
-			map.wait();
-			map.putIfAbsent(key, value);
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			map.put(key, value);
+			System.out.format("Thread %s includes key %d value %s current size %d %n", Thread.currentThread().getName(), key, value, map.size());
+		} catch (Exception e) {
+			System.out.format("%s %n", e.getMessage());
 		}
 	}
-	
+
 	public static void main(String[] args) {
-		ExecutorService ex = Executors.newFixedThreadPool(100);
-		final P03 p = new P03();
+		ConcurrentHashMap<Integer, String> map = new ConcurrentHashMap<>();
+		ExecutorService ex = Executors.newFixedThreadPool(2);
 		try {
 			for (int i = 0; i < 10; i++) {
-				System.out.println(p.map.size());
-				ex.submit(new Runnable() {
-					int x = p.map.size();
-					@Override
-					public void run() {
-						p.putToMap(Integer.toString(x), Integer.toString(x));
-					}
-				});
+				ex.submit(new P03(map,i,Integer.toString(i)));
 			}
-			
 		} finally {
 			ex.shutdown();
 		}
 		
-		for (Entry<String, String> s : p.map.entrySet()) {
-			System.out.println(s.getKey() + "," + s.getValue());
+		try {
+			ex.awaitTermination(Long.MAX_VALUE, TimeUnit.DAYS);
+		} catch (InterruptedException e) {
+			System.out.format("%s %n", e.getMessage());
+			
 		}
+		List<Integer> sortedMap = new ArrayList<>(map.keySet());
+		Collections.sort(sortedMap);
+		for (int s : sortedMap) {
+			System.out.format("%d,%s %n",s,map.get(s));
+		}
+		System.out.format("count %d %n", map.size());
 	}
+
 }
