2fd1e83fd6558ecb600185463c60b724a0bfe3c7
zoudalie <uwe.dalies@zeiss.com>
2023-04-29 16:59:12 +0200

    string literal

---
+++ oracleOCP/source/dalieOCP/string/literal/Execute.java
@@ -0,0 +1,11 @@
+package dalieOCP.string.literal;
+
+public class Execute {
+    /**
+     * @param args
+     */
+    public final static void main(String[] args){
+        final String s =  "\"\"\"\"\"\"\"\"\"\"";
+        System.out.println(s);
+    }
+}
