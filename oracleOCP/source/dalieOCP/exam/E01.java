eedb8034068ba716119889959aed471c7a0ed4ab
zoudalie <uwe.dalies@zeiss.com>
2023-05-06 08:28:21 +0200

    Update E01.java

--- oracleOCP/source/dalieOCP/exam/E01.java
+++ oracleOCP/source/dalieOCP/exam/E01.java
@@ -10,7 +10,7 @@ final class E01 {
 	public static void main(String[] args) {
 		Object[] s = {"A","B","C",null};
 		String s1 = "Hello" , s2 = "World";
-		out.format("%2$s %s %1$s %4$b", s);
+		out.format("%2$s %1$s %4$b", s);
 		out.format("%n%2$s %s", s1,s2);
 	}
 }
